dafb9cdc3c7f9ee89387729b4b767ccaa5e17714
Wesley Lin <wesleylin625@gmail.com>
2026-01-02 13:03:14 -0800

    add commemtns

--- heap/heap_fluent.go
+++ heap/heap_fluent.go
@@ -1,11 +1,12 @@
 package heap
 
+// Push is a fluent version of Insert, adding an element to the heap. Returns the heap for chaining.
 func (h *Heap[T, P]) Push(val T, priority P) *Heap[T, P] {
 	h.Insert(val, priority)
 	return h
 }
 
-// Drop is a fluent version of Pop, removing and discarding the top element of the heap.
+// Drop is a fluent version of Pop, removing and discarding the top element of the heap. Returns the heap for chaining.
 func (h *Heap[T, P]) Drop() *Heap[T, P] {
 	h.Pop()
 	return h
