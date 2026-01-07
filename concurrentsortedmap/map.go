b3b27ce51dfd4df4fb130601919c3d35c8de5256
Wesley Lin <wesleylin625@gmail.com>
2026-01-07 01:36:17 -0800

    added get function

--- concurrentsortedmap/map.go
+++ concurrentsortedmap/map.go
@@ -4,12 +4,14 @@ import (
 	"cmp"
 	"sync"
 
+	"github.com/wesleylin/basin/internal/hash"
 	"github.com/wesleylin/basin/sortedmap"
 )
 
 const shardCount = 256
+const shardMask = shardCount - 1
 
-type ConcurrentSortedMap[K cmp.Ordered, V any] struct {
+type Map[K cmp.Ordered, V any] struct {
 	// shardMask is (shardCount - 1). Used for fast bitwise indexing.
 	shardMask uint64
 	shards    [shardCount]*shard[K, V]
@@ -21,8 +23,8 @@ type shard[K cmp.Ordered, V any] struct {
 	data sortedmap.SortedMap[K, V]
 }
 
-func New[K cmp.Ordered, V any]() *ConcurrentSortedMap[K, V] {
-	cm := &ConcurrentSortedMap[K, V]{
+func New[K cmp.Ordered, V any]() *Map[K, V] {
+	cm := &Map[K, V]{
 		shardMask: shardCount - 1,
 	}
 
@@ -32,6 +34,21 @@ func New[K cmp.Ordered, V any]() *ConcurrentSortedMap[K, V] {
 		}
 	}
 	return cm
+}
+
+func (m *Map[K, V]) getShard(key K) *shard[K, V] {
+	h := hash.Maphash(key)
+	return m.shards[h&uint64(shardMask)]
+}
+
+// Get retrieves a value from the correct shard
+func (m *Map[K, V]) Get(key K) (V, bool) {
+	s := m.getShard(key)
+	s.RLock()
+	defer s.RUnlock()
+
+	res, ok := s.data.Get(key)
+	return res, ok
 }
 
 // shard has a pointer receiver because it must lock the mutex
